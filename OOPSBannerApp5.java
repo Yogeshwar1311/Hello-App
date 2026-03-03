f425cf499462cf2f1904311ef9561495cb96a07e
Yogeshwar1311 <yogeshwarvs247@gmail.com>
2026-03-03 09:01:36 +0530

    coded uc5 to print oops

---
+++ OOPSBannerApp5.java
@@ -0,0 +1,19 @@
+public class OOPSBannerApp5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            " ***     ***    ******     ******",
+            "**  **   **  **   **   **   **   **",
+            "**  **   **  **   **   **   **   **",
+            "**  **   **  **   ******     ******",
+            "**  **   **  **   **              **",
+            "**  **   **  **   **              **",
+            " ***     ***       **         ******"
+        };
+
+        for (int i = 0; i < lines.length; i++) {
+            System.out.println(lines[i]);
+        }
+    }
+}
